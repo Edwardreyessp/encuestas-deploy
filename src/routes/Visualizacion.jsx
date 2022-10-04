4ee3acf517518633466f7a0b731c57b365871976
Edwardreyessp <edwardreyessp@gmail.com>
2022-10-03 21:52:43 -0500

    Agregación de carpeta components

--- src/routes/Visualizacion.jsx
+++ src/routes/Visualizacion.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
-import Files from '../components/visualizacion/Files';
-import Navbar from '../components/Navbar';
-import Questions from '../components/visualizacion/Questions';
+import Files from '../pages/visualizacion/Files';
+import Navbar from '../pages/components/Navbar';
+import Questions from '../pages/visualizacion/Questions';
 
 const Visualizacion = () => {
   const [done, setDone] = useState(false);
