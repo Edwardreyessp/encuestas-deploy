ef78ec4c1ed652c3f53995bc94ee924eb5737647
Mohacyr Rojas <mohacyr.rojas.contractor@bbva.com>
2022-10-04 21:42:57 -0500

    chore(*): change directory structure

--- src/routes/Posestratificacion.jsx
+++ src/routes/Posestratificacion.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
-import Navbar from '../pages/components/Navbar';
-import Data from '../pages/posestratificacion/Data';
-import Files from '../pages/posestratificacion/Files';
+import Navbar from '../components/utils/Navbar';
+import Data from '../components/posestratificacion/Data';
+import Files from '../components/posestratificacion/Files';
 
 const Posestratificacion = () => {
   const [done, setDone] = useState(false);
