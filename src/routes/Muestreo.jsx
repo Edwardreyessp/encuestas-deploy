4ab15953c68a692cbea8c7a92ea36a8cc3f6cb4d
Mohacyr Rojas <mohacyr.rojas.contractor@bbva.com>
2022-10-05 00:04:42 -0500

    feat(*): add uppy and test

--- src/routes/Muestreo.jsx
+++ src/routes/Muestreo.jsx
@@ -1,12 +1,28 @@
+import { useMemo } from 'react';
 import Navbar from '../components/utils/Navbar';
+import Uppy from '@uppy/core';
+import { Dashboard } from '@uppy/react';
+import '@uppy/core/dist/style.css';
+import '@uppy/dashboard/dist/style.css';
 
-const Visualizacion = () => {
+const Muestreo = () => {
+  const uppy = useMemo(() => {
+    return new Uppy({
+      id: 'uppy1',
+      autoProceed: false,
+      restrictions: {
+        allowedFileTypes: ['.mp4'],
+      },
+      allowMultipleUploads: false,
+    });
+  }, []);
   return (
     <>
       <Navbar />
       <p>test</p>
+      <Dashboard uppy={uppy} />
     </>
   );
 };
 
-export default Visualizacion;
+export default Muestreo;
