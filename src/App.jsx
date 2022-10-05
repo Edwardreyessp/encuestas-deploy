5089b22bf4abe805a5b4de80c29a6e39a06adc71
Mohacyr Rojas <mohacyr.rojas.contractor@bbva.com>
2022-10-04 23:10:54 -0500

    feat(routes): add muestreo route
    close #15

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import ReactDOM from 'react-dom/client';
 import Visualizacion from './routes/Visualizacion';
 import Posestratificacion from './routes/Posestratificacion';
+import Muestreo from './routes/Muestreo';
 import './main.scss';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
+      <Route path="/muestreo" element={<Muestreo />} />
       <Route path="/posestratificacion" element={<Posestratificacion />} />
       <Route path="/visualizacion" element={<Visualizacion />} />
       <Route path="/" element={<Visualizacion />} />
---
+++ src/routes/Muestreo.jsx
@@ -0,0 +1,12 @@
+import Navbar from '../components/utils/Navbar';
+
+const Visualizacion = () => {
+  return (
+    <>
+      <Navbar />
+      <p>test</p>
+    </>
+  );
+};
+
+export default Visualizacion;
