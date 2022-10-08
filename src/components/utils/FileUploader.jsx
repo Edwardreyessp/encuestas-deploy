0e52951156ba17c45c04c20630ad93989bfe969b
Mohacyr Rojas <mohacyr.rojas.contractor@bbva.com>
2022-10-08 16:43:26 -0500

    feat(FileUploader): render list of selected files

--- src/components/utils/FileUploader.jsx
+++ src/components/utils/FileUploader.jsx
@@ -1,27 +1,75 @@
+import {
+  Button,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+} from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
 import { useState } from 'react';
 
 const FileUploader = () => {
   const [files, setFiles] = useState([]);
 
+  /**
+   * Handle uploaded files
+   * @event
+   * @param {event} event - default event trigerred by the file input
+   */
   function addFileHandler(event) {
     addFiles(event.target.files);
   }
 
+  /**
+   * Manage added files
+   * @function
+   * @param {FileObject} fileObject - Web API file object with uploaded files
+   */
   const addFiles = fileObject => {
     for (let index = 0; index < fileObject.length; index++) {
       setFiles(curr => [...curr, fileObject[index]]);
     }
   };
 
+  /**
+   * Generates material ui list items
+   * @function
+   * @param {array} list - The array of items to list
+   * @param {string} keyIdentifier - A identifier of the list
+   * @param {@mui/icon} icon - An action icon
+   * TODO: allows selection of the text property
+   */
+  function generateListItems(list, keyIdentifier, icon) {
+    return list.map((file, i) => {
+      return (
+        <ListItem
+          key={i + keyIdentifier}
+          secondaryAction={<IconButton edge="end">{icon}</IconButton>}
+        >
+          <ListItemText primary={file.name} />
+        </ListItem>
+      );
+    });
+  }
+
   return (
     <form>
       <div className="preview">
-        {files.map((file, i) => (
-          <p key={i}>{file.name}</p>
-        ))}
+        <List>
+          {generateListItems(files, '-uploaded-files', <DeleteIcon />)}
+          <ListItem
+            secondaryAction={
+              <IconButton edge="end" aria-label="delete">
+                <DeleteIcon />
+              </IconButton>
+            }
+          >
+            <ListItemText primary="test" />
+          </ListItem>
+        </List>
       </div>
       <input multiple type="file" name="file" onChange={addFileHandler} />
-      <button type="submit">Enviar</button>
+      <Button variant="contained">Enviar</Button>
     </form>
   );
 };
