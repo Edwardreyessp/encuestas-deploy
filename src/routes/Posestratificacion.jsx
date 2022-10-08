16de6acb9778e2fa2cee0aa1c23715e938941089
Edwardreyessp <edwardreyessp@gmail.com>
2022-10-08 18:01:56 -0500

    Inicio de Posestratificacion

--- src/routes/Posestratificacion.jsx
+++ src/routes/Posestratificacion.jsx
@@ -2,14 +2,35 @@ import { useState } from 'react';
 import Navbar from '../components/utils/Navbar';
 import Data from '../components/posestratificacion/Data';
 import Files from '../components/posestratificacion/Files';
+import MyStepper from '../components/utils/MyStepper';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
 
 const Posestratificacion = () => {
-  const [done, setDone] = useState(false);
+  const [step, setStep] = useState(0);
+  const steps = ['Subir archivos', 'Configurar estratos', 'Llenar estratos'];
 
   return (
     <>
       <Navbar />
-      {done ? <Data /> : <Files setDone={setDone} />}
+      <Box width={'100%'}>
+        <Stack>
+          <Box margin={'36px 0'} width={'880px'}>
+            <MyStepper steps={steps} activeStep={step} />
+          </Box>
+          {
+            {
+              1: <Files />,
+              2: <Data />,
+              3: <Files />,
+            }[step]
+          }
+          <Box display={'flex'} justifyContent={'flex-end'}>
+            <Button onClick={() => setStep(step + 1)}>Continuar</Button>
+          </Box>
+        </Stack>
+      </Box>
     </>
   );
 };
