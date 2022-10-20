ba6da33e4e6783df820a38c83c636b027d891295
Edwardreyessp <edwardreyessp@gmail.com>
2022-10-20 00:24:26 -0500

    temp: FileUploader temporal

--- src/routes/Visualizacion.jsx
+++ src/routes/Visualizacion.jsx
@@ -4,6 +4,7 @@ import FileUploader from '../components/utils/FileUploader';
 import Questions from '../components/visualizacion/Question';
 import { Button, Box } from '@mui/material';
 import { useState } from 'react';
+import { uploadFiles } from '../services/Index';
 
 /**
  * Módulo visualización
@@ -12,6 +13,24 @@ import { useState } from 'react';
 const Visualizacion = () => {
   const [step, setStep] = useState(0);
   const steps = ['Subir archivos', 'Configurar gráficas'];
+  const fileTypes = { word: ['.docx'], excel: ['.csv'], power: ['.pptx'] };
+
+  /**
+   * Temp FileUploader
+   * @function
+   */
+  const handleUploadFiles = async () => {
+    const files = {
+      word: 'https://firebasestorage.googleapis.com/v0/b/proyectoencuestas1-f2ece.appspot.com/o/cuestionario_2.docx?alt=media&token=8ebcb95e-2de2-449f-9c0d-f64a3f3c8fd4',
+      excel:
+        'https://firebasestorage.googleapis.com/v0/b/proyectoencuestas1-f2ece.appspot.com/o/data_ejemplo.csv?alt=media&token=59b20a31-5afe-4590-8c93-1cffa25911a1',
+      power:
+        'https://firebasestorage.googleapis.com/v0/b/proyectoencuestas1-f2ece.appspot.com/o/default_pptx%2Fdefault.pptx?alt=media&token=918daf7b-396f-48f7-a7b6-0ef22a0e317a',
+    };
+    const response = await uploadFiles(files, 'files');
+    console.log(response.data);
+    setStep(step + 1);
+  };
 
   return (
     <>
@@ -28,7 +47,7 @@ const Visualizacion = () => {
           </Box>
           {
             {
-              0: <FileUploader />,
+              0: <FileUploader fileTypes={fileTypes} numberOfFiles={3} />,
               1: <Questions />,
             }[step]
           }
@@ -36,11 +55,7 @@ const Visualizacion = () => {
       </Box>
       {step === 0 ? (
         <Box display={'flex'} justifyContent={'flex-end'} mr={8} mb={3}>
-          <Button
-            size="medium"
-            variant="contained"
-            onClick={() => setStep(step + 1)}
-          >
+          <Button size="medium" variant="contained" onClick={handleUploadFiles}>
             Siguiente
           </Button>
         </Box>
