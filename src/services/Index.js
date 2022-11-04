186b7be4513591d25e538948c13b4baa593cb938
Edwardreyessp <edwardreyessp@gmail.com>
2022-11-04 15:22:25 -0600

    feat: Configuración Visualización

--- src/services/Index.js
+++ src/services/Index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const baseUrl = 'https://encuestas1.herokuapp.com';
+// const baseUrl = 'https://encuestas1.herokuapp.com';
+const baseUrl = 'http://127.0.0.1:4000';
 
 /**
  * Obtiene las preguntas del archivo subido
