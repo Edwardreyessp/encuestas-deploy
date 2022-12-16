cf16742988b6d38a36ffb344f1447d3b995f5f7b
Edwardreyessp <edwardreyessp@gmail.com>
2022-12-16 02:24:52 -0600

    refactor: Quitar navbar sin login

--- src/routes/SingIn.jsx
+++ src/routes/SingIn.jsx
@@ -10,7 +10,6 @@ import {
   Typography,
 } from '@mui/material';
 import googleIcon from '../assets/images/google.svg';
-import Navbar from '../components/utils/Navbar';
 // Icons
 import MailOutlinedIcon from '@mui/icons-material/MailOutlined';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
@@ -46,134 +45,131 @@ const SingIn = () => {
   };
 
   return (
-    <>
-      <Navbar current={'singin'} />
-      <ThemeProvider theme={theme}>
-        <Box display={'flex'} height="calc(100vh - 64px)" width={'100vw'}>
-          <Stack
-            alignItems="center"
-            justifyContent="center"
-            paddingX={'88px'}
-            spacing={2}
-            width="100%"
-          >
-            <Typography fontSize={40} color="primary" fontWeight={700}>
-              Inicia Sesión
-            </Typography>
-            <Button
-              variant="contained"
-              sx={{ width: '376px' }}
-              startIcon={
-                <img
-                  width={23}
-                  style={{ borderRadius: '2px' }}
-                  src={googleIcon}
-                />
-              }
-            >
-              Iniciar sesión con Google
-            </Button>
-            <Typography color={theme.palette.mygray.main}>
-              O usa tu cuenta de correo:
-            </Typography>
-            <InputStartIcon
-              placeholder={'Correo'}
-              value={email}
-              onChange={event => {
-                setEmail(event.target.value);
-              }}
-              icon={
-                <MailOutlinedIcon sx={{ color: theme.palette.mygray.main }} />
-              }
-            />
-            <Paper
-              elevation={2}
-              sx={{
-                p: '2px 10px',
-                display: 'flex',
-                alignItems: 'center',
-                width: 376,
-                minHeight: 55,
-                bgcolor: '#F4F8F7',
-              }}
-            >
-              <LockOutlinedIcon sx={{ color: theme.palette.mygray.main }} />
-              <TextField
-                sx={{ ml: 1, input: { bgcolor: '#F4F8F7' } }}
-                label="Contraseña"
-                variant="filled"
-                type={showPassword ? 'text' : 'password'}
-                fullWidth
-                value={password}
-                onChange={event => {
-                  setPassword(event.target.value);
-                }}
-                InputProps={{
-                  disableUnderline: true,
-                }}
+    <ThemeProvider theme={theme}>
+      <Box display={'flex'} height="100vh" width={'100vw'}>
+        <Stack
+          alignItems="center"
+          justifyContent="center"
+          paddingX={'88px'}
+          spacing={2}
+          width="100%"
+        >
+          <Typography fontSize={40} color="primary" fontWeight={700}>
+            Inicia Sesión
+          </Typography>
+          <Button
+            variant="contained"
+            sx={{ width: '376px' }}
+            startIcon={
+              <img
+                width={23}
+                style={{ borderRadius: '2px' }}
+                src={googleIcon}
               />
-              <IconButton onClick={() => setShowPassword(!showPassword)}>
-                {showPassword ? (
-                  <VisibilityOff sx={{ color: theme.palette.mygray.main }} />
-                ) : (
-                  <Visibility sx={{ color: theme.palette.mygray.main }} />
-                )}
-              </IconButton>
-            </Paper>
-            <Typography sx={{ cursor: 'pointer' }}>
-              ¿Olvidaste tu contraseña?
-            </Typography>
-            <Button
-              variant="contained"
-              sx={{ borderRadius: '50px', mt: 100 }}
-              onClick={logIn}
-            >
-              INICIAR SESIÓN
-            </Button>
-          </Stack>
-          <Stack
-            sx={{ bgcolor: theme.palette.primary.main }}
-            alignItems="center"
-            justifyContent="center"
-            paddingX={'88px'}
-            spacing={2}
+            }
           >
-            <Typography
-              color={theme.palette.secondary.main}
-              fontSize={40}
-              fontWeight={700}
-              width="405px"
-              textAlign={'center'}
+            Iniciar sesión con Google
+          </Button>
+          <Typography color={theme.palette.mygray.main}>
+            O usa tu cuenta de correo:
+          </Typography>
+          <InputStartIcon
+            placeholder={'Correo'}
+            value={email}
+            onChange={event => {
+              setEmail(event.target.value);
+            }}
+            icon={
+              <MailOutlinedIcon sx={{ color: theme.palette.mygray.main }} />
+            }
+          />
+          <Paper
+            elevation={2}
+            sx={{
+              p: '2px 10px',
+              display: 'flex',
+              alignItems: 'center',
+              width: 376,
+              minHeight: 55,
+              bgcolor: '#F4F8F7',
+            }}
+          >
+            <LockOutlinedIcon sx={{ color: theme.palette.mygray.main }} />
+            <TextField
+              sx={{ ml: 1, input: { bgcolor: '#F4F8F7' } }}
+              label="Contraseña"
+              variant="filled"
+              type={showPassword ? 'text' : 'password'}
+              fullWidth
+              value={password}
+              onChange={event => {
+                setPassword(event.target.value);
+              }}
+              InputProps={{
+                disableUnderline: true,
+              }}
+            />
+            <IconButton onClick={() => setShowPassword(!showPassword)}>
+              {showPassword ? (
+                <VisibilityOff sx={{ color: theme.palette.mygray.main }} />
+              ) : (
+                <Visibility sx={{ color: theme.palette.mygray.main }} />
+              )}
+            </IconButton>
+          </Paper>
+          <Typography sx={{ cursor: 'pointer' }}>
+            ¿Olvidaste tu contraseña?
+          </Typography>
+          <Button
+            variant="contained"
+            sx={{ borderRadius: '50px', mt: 100 }}
+            onClick={logIn}
+          >
+            INICIAR SESIÓN
+          </Button>
+        </Stack>
+        <Stack
+          sx={{ bgcolor: theme.palette.primary.main }}
+          alignItems="center"
+          justifyContent="center"
+          paddingX={'88px'}
+          spacing={2}
+        >
+          <Typography
+            color={theme.palette.secondary.main}
+            fontSize={40}
+            fontWeight={700}
+            width="405px"
+            textAlign={'center'}
+          >
+            ¡Bienvenido!
+          </Typography>
+          <Typography
+            fontSize={20}
+            color="secondary"
+            width={'395px'}
+            textAlign="center"
+          >
+            Ingresa tus datos y regístrate para usar todos los servicios.
+          </Typography>
+          <Button
+            variant="outlined"
+            color="secondary"
+            sx={{ borderRadius: '50px' }}
+          >
+            <Link
+              to={'/sing-up'}
+              style={{
+                textDecoration: 'none',
+                color: theme.palette.secondary.main,
+              }}
             >
-              ¡Bienvenido!
-            </Typography>
-            <Typography
-              fontSize={20}
-              color="secondary"
-              width={'395px'}
-              textAlign="center"
-            >
-              Ingresa tus datos y regístrate para usar todos los servicios.
-            </Typography>
-            <Button
-              variant="outlined"
-              color="secondary"
-              sx={{ borderRadius: '50px' }}
-            >
-              <Link
-                to={'/sing-up'}
-                style={{
-                  textDecoration: 'none',
-                  color: theme.palette.secondary.main,
-                }}
-              >
-                REGÍSTRATE
-              </Link>
-            </Button>
-          </Stack>
-        </Box>
-      </ThemeProvider>
-    </>
+              REGÍSTRATE
+            </Link>
+          </Button>
+        </Stack>
+      </Box>
+    </ThemeProvider>
   );
 };
 
--- src/routes/SingUp.jsx
+++ src/routes/SingUp.jsx
@@ -10,7 +10,6 @@ import {
   Typography,
 } from '@mui/material';
 import googleIcon from '../assets/images/google.svg';
-import Navbar from '../components/utils/Navbar';
 // Icons
 import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
 import MailOutlinedIcon from '@mui/icons-material/MailOutlined';
@@ -58,138 +57,135 @@ const SingUp = () => {
   };
 
   return (
-    <>
-      <Navbar current={'singin'} />
-      <ThemeProvider theme={theme}>
-        <Box display={'flex'} height="calc(100vh - 64px)" width={'100vw'}>
-          <Stack
-            sx={{ bgcolor: theme.palette.primary.main }}
-            alignItems="center"
-            justifyContent="center"
-            paddingX={'88px'}
-            spacing={2}
+    <ThemeProvider theme={theme}>
+      <Box display={'flex'} height="100vh" width={'100vw'}>
+        <Stack
+          sx={{ bgcolor: theme.palette.primary.main }}
+          alignItems="center"
+          justifyContent="center"
+          paddingX={'88px'}
+          spacing={2}
+        >
+          <Typography
+            color={'white'}
+            fontSize={40}
+            fontWeight={700}
+            width="405px"
           >
-            <Typography
-              color={'white'}
-              fontSize={40}
-              fontWeight={700}
-              width="405px"
-            >
-              ¡Bienvenido de vuelta!
-            </Typography>
-            <Typography
-              fontSize={20}
-              color="white"
-              width={'395px'}
-              textAlign="center"
-            >
-              Para mantenerse conectado, por favor inicia sesión con tu cuenta
-              personal.
-            </Typography>
+            ¡Bienvenido de vuelta!
+          </Typography>
+          <Typography
+            fontSize={20}
+            color="white"
+            width={'395px'}
+            textAlign="center"
+          >
+            Para mantenerse conectado, por favor inicia sesión con tu cuenta
+            personal.
+          </Typography>
 
-            <Button
-              variant="outlined"
-              color="contrast"
-              sx={{ borderRadius: '50px' }}
-            >
-              <Link
-                to={'/sing-in'}
-                style={{ textDecoration: 'none', color: 'white' }}
-              >
-                INICIAR SESIÓN
-              </Link>
-            </Button>
-          </Stack>
-          <Stack
-            alignItems="center"
-            justifyContent="center"
-            paddingX={'88px'}
-            spacing={2}
-            width="100%"
+          <Button
+            variant="outlined"
+            color="contrast"
+            sx={{ borderRadius: '50px' }}
           >
-            <Typography fontSize={40} color="primary" fontWeight={700}>
-              Crea una Cuenta
-            </Typography>
-            <Button
-              variant="contained"
-              sx={{ width: '376px' }}
-              startIcon={
-                <img
-                  width={23}
-                  style={{ borderRadius: '2px' }}
-                  src={googleIcon}
-                />
-              }
+            <Link
+              to={'/sing-in'}
+              style={{ textDecoration: 'none', color: 'white' }}
             >
-              Iniciar sesión con Google
-            </Button>
-            <Typography color={theme.palette.mygray.main}>
-              O usa un correo para registrarte:
-            </Typography>
-            <InputStartIcon
-              placeholder={'Nombre'}
-              value={user.name}
-              name={'name'}
-              onChange={handleUpdate}
-              icon={
-                <PersonOutlineOutlinedIcon
-                  sx={{ color: theme.palette.mygray.main }}
-                />
-              }
-            />
-            <InputStartIcon
-              placeholder={'Correo'}
-              value={user.email}
-              name={'email'}
-              onChange={handleUpdate}
-              icon={
-                <MailOutlinedIcon sx={{ color: theme.palette.mygray.main }} />
-              }
-            />
-            <Paper
-              elevation={2}
-              sx={{
-                p: '2px 10px',
-                display: 'flex',
-                alignItems: 'center',
-                width: 376,
-                minHeight: 55,
-                bgcolor: '#F4F8F7',
-              }}
-            >
-              <LockOutlinedIcon sx={{ color: theme.palette.mygray.main }} />
-              <TextField
-                sx={{ ml: 1, input: { bgcolor: '#F4F8F7' } }}
-                label="Contraseña"
-                name="password"
-                variant="filled"
-                type={showPassword ? 'text' : 'password'}
-                fullWidth
-                value={user.password}
-                onChange={handleUpdate}
-                InputProps={{
-                  disableUnderline: true,
-                }}
+              INICIAR SESIÓN
+            </Link>
+          </Button>
+        </Stack>
+        <Stack
+          alignItems="center"
+          justifyContent="center"
+          paddingX={'88px'}
+          spacing={2}
+          width="100%"
+        >
+          <Typography fontSize={40} color="primary" fontWeight={700}>
+            Crea una Cuenta
+          </Typography>
+          <Button
+            variant="contained"
+            sx={{ width: '376px' }}
+            startIcon={
+              <img
+                width={23}
+                style={{ borderRadius: '2px' }}
+                src={googleIcon}
               />
-              <IconButton onClick={() => setShowPassword(!showPassword)}>
-                {showPassword ? (
-                  <VisibilityOff sx={{ color: theme.palette.mygray.main }} />
-                ) : (
-                  <Visibility sx={{ color: theme.palette.mygray.main }} />
-                )}
-              </IconButton>
-            </Paper>
-            <Button
-              variant="contained"
-              sx={{ borderRadius: '50px', mt: 100 }}
-              onClick={createUser}
-            >
-              REGISTRARSE
-            </Button>
-          </Stack>
-        </Box>
-      </ThemeProvider>
-    </>
+            }
+          >
+            Iniciar sesión con Google
+          </Button>
+          <Typography color={theme.palette.mygray.main}>
+            O usa un correo para registrarte:
+          </Typography>
+          <InputStartIcon
+            placeholder={'Nombre'}
+            value={user.name}
+            name={'name'}
+            onChange={handleUpdate}
+            icon={
+              <PersonOutlineOutlinedIcon
+                sx={{ color: theme.palette.mygray.main }}
+              />
+            }
+          />
+          <InputStartIcon
+            placeholder={'Correo'}
+            value={user.email}
+            name={'email'}
+            onChange={handleUpdate}
+            icon={
+              <MailOutlinedIcon sx={{ color: theme.palette.mygray.main }} />
+            }
+          />
+          <Paper
+            elevation={2}
+            sx={{
+              p: '2px 10px',
+              display: 'flex',
+              alignItems: 'center',
+              width: 376,
+              minHeight: 55,
+              bgcolor: '#F4F8F7',
+            }}
+          >
+            <LockOutlinedIcon sx={{ color: theme.palette.mygray.main }} />
+            <TextField
+              sx={{ ml: 1, input: { bgcolor: '#F4F8F7' } }}
+              label="Contraseña"
+              name="password"
+              variant="filled"
+              type={showPassword ? 'text' : 'password'}
+              fullWidth
+              value={user.password}
+              onChange={handleUpdate}
+              InputProps={{
+                disableUnderline: true,
+              }}
+            />
+            <IconButton onClick={() => setShowPassword(!showPassword)}>
+              {showPassword ? (
+                <VisibilityOff sx={{ color: theme.palette.mygray.main }} />
+              ) : (
+                <Visibility sx={{ color: theme.palette.mygray.main }} />
+              )}
+            </IconButton>
+          </Paper>
+          <Button
+            variant="contained"
+            sx={{ borderRadius: '50px', mt: 100 }}
+            onClick={createUser}
+          >
+            REGISTRARSE
+          </Button>
+        </Stack>
+      </Box>
+    </ThemeProvider>
   );
 };
 
