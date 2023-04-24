63aabc09376732ea2db263dea9613bcd79a0991c
Edwardreyessp <edwardreyessp@gmail.com>
2023-04-24 15:10:45 -0600

    feat: Hint of error visualización

--- src/components/visualizacion/Visual.jsx
+++ src/components/visualizacion/Visual.jsx
@@ -46,6 +46,59 @@ const Visual = () => {
       <Stack spacing={2} alignItems="center">
         <Box>No se recibió la respuesta esperada de servidor</Box>
         <Box>Revisar consola para más info</Box>
+        <Box>Respuesta esperada: ?Depende del tipo de pregunta</Box>
+        <Box>
+          <Box>{'{'}</Box>
+          <Box>&nbsp;&nbsp;{'  "001Q_3_1": {'}</Box>
+          <Box>&nbsp;&nbsp;&nbsp;&nbsp;{'    "id": "001Q_3_1",'}</Box>
+          <Box>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            {'    "enunciado": "Enunciado de la pregunta 1",'}
+          </Box>
+          <Box>&nbsp;&nbsp;&nbsp;&nbsp;{'    "tipo": "bq",'}</Box>
+          <Box>&nbsp;&nbsp;&nbsp;&nbsp;{'    "respuestas": {'}</Box>
+          <Box>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            {
+              '      "1": {color: "#000fff, orden: "0", respuesta: "Respuesta 1", activo?: "true},'
+            }
+          </Box>
+          <Box>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            {
+              '      "2": {color: "#000fff, orden: "0", respuesta: "Respuesta 2", activo?: "true},'
+            }
+          </Box>
+          <Box>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            {
+              '      "3": {color: "#000fff, orden: "0", respuesta: "Respuesta 3", activo?: "true}'
+            }
+          </Box>
+          <Box>&nbsp;&nbsp;&nbsp;&nbsp;{'    },'}</Box>
+          <Box>&nbsp;&nbsp;&nbsp;&nbsp;{'    "categorías?": {'}</Box>
+          <Box>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            {
+              '      "1": {color: "#000fff, orden: "0", respuesta: "Respuesta 1", activo: "true"},'
+            }
+          </Box>
+          <Box>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            {
+              '      "2": {color: "#000fff, orden: "0", respuesta: "Respuesta 2", activo: "true"},'
+            }
+          </Box>
+          <Box>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            {
+              '      "3": {color: "#000fff, orden: "0", respuesta: "Respuesta 3", activo: "true"}'
+            }
+          </Box>
+          <Box>&nbsp;&nbsp;&nbsp;&nbsp;{'    }'}</Box>
+          <Box>&nbsp;&nbsp;{'  }'}</Box>
+          <Box>{'}'}</Box>
+        </Box>
       </Stack>
     );
   }
